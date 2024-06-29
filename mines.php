362b6c5829805bd07edc680e0a301f2a4cb914a2
ZalohaD <dinis7794@gmail.com>
2024-06-29 22:06:43 +0300

    Minor fix

--- mines.php
+++ mines.php
@@ -27,103 +27,39 @@
 
 <table>
     <tr>
-        <th></th>
-        <th>1</th>
-        <th>2</th>
-        <th>3</th>
-        <th>4</th>
-        <th>5</th>
-        <th>6</th>
-        <th>7</th>
-        <th>8</th>
+        <td class="hidden"></td>
+        <td class="hidden"></td>
+        <td class="hidden"></td>
+        <td class="hidden"></td>
+        <td class="hidden"></td>
     </tr>
     <tr>
-        <th>A</th>
-        <td class="hidden" data-row="A" data-col="1"></td>
-        <td class="hidden" data-row="A" data-col="2"></td>
-        <td class="hidden" data-row="A" data-col="3"></td>
-        <td class="hidden" data-row="A" data-col="4"></td>
-        <td class="hidden" data-row="A" data-col="5"></td>
-        <td class="hidden" data-row="A" data-col="6"></td>
-        <td class="hidden" data-row="A" data-col="7"></td>
-        <td class="hidden" data-row="A" data-col="8"></td>
+        <td class="hidden"></td>
+        <td class="hidden"></td>
+        <td class="hidden"></td>
+        <td class="hidden"></td>
+        <td class="hidden"></td>
     </tr>
     <tr>
-        <th>B</th>
-        <td class="hidden" data-row="B" data-col="1"></td>
-        <td class="hidden" data-row="B" data-col="2"></td>
-        <td class="hidden" data-row="B" data-col="3"></td>
-        <td class="hidden" data-row="B" data-col="4"></td>
-        <td class="hidden" data-row="B" data-col="5"></td>
-        <td class="hidden" data-row="B" data-col="6"></td>
-        <td class="hidden" data-row="B" data-col="7"></td>
-        <td class="hidden" data-row="B" data-col="8"></td>
+        <td class="hidden"></td>
+        <td class="hidden"></td>
+        <td class="hidden"></td>
+        <td class="hidden"></td>
+        <td class="hidden"></td>
     </tr>
     <tr>
-        <th>C</th>
-        <td class="hidden" data-row="C" data-col="1"></td>
-        <td class="hidden" data-row="C" data-col="2"></td>
-        <td class="hidden" data-row="C" data-col="3"></td>
-        <td class="hidden" data-row="C" data-col="4"></td>
-        <td class="hidden" data-row="C" data-col="5"></td>
-        <td class="hidden" data-row="C" data-col="6"></td>
-        <td class="hidden" data-row="C" data-col="7"></td>
-        <td class="hidden" data-row="C" data-col="8"></td>
+        <td class="hidden"></td>
+        <td class="hidden"></td>
+        <td class="hidden"></td>
+        <td class="hidden"></td>
+        <td class="hidden"></td>
     </tr>
     <tr>
-        <th>D</th>
-        <td class="hidden" data-row="D" data-col="1"></td>
-        <td class="hidden" data-row="D" data-col="2"></td>
-        <td class="hidden" data-row="D" data-col="3"></td>
-        <td class="hidden" data-row="D" data-col="4"></td>
-        <td class="hidden" data-row="D" data-col="5"></td>
-        <td class="hidden" data-row="D" data-col="6"></td>
-        <td class="hidden" data-row="D" data-col="7"></td>
-        <td class="hidden" data-row="D" data-col="8"></td>
-    </tr>
-    <tr>
-        <th>E</th>
-        <td class="hidden" data-row="E" data-col="1"></td>
-        <td class="hidden" data-row="E" data-col="2"></td>
-        <td class="hidden" data-row="E" data-col="3"></td>
-        <td class="hidden" data-row="E" data-col="4"></td>
-        <td class="hidden" data-row="E" data-col="5"></td>
-        <td class="hidden" data-row="E" data-col="6"></td>
-        <td class="hidden" data-row="E" data-col="7"></td>
-        <td class="hidden" data-row="E" data-col="8"></td>
-    </tr>
-    <tr>
-        <th>F</th>
-        <td class="hidden" data-row="F" data-col="1"></td>
-        <td class="hidden" data-row="F" data-col="2"></td>
-        <td class="hidden" data-row="F" data-col="3"></td>
-        <td class="hidden" data-row="F" data-col="4"></td>
-        <td class="hidden" data-row="F" data-col="5"></td>
-        <td class="hidden" data-row="F" data-col="6"></td>
-        <td class="hidden" data-row="F" data-col="7"></td>
-        <td class="hidden" data-row="F" data-col="8"></td>
-    </tr>
-    <tr>
-        <th>G</th>
-        <td class="hidden" data-row="G" data-col="1"></td>
-        <td class="hidden" data-row="G" data-col="2"></td>
-        <td class="hidden" data-row="G" data-col="3"></td>
-        <td class="hidden" data-row="G" data-col="4"></td>
-        <td class="hidden" data-row="G" data-col="5"></td>
-        <td class="hidden" data-row="G" data-col="6"></td>
-        <td class="hidden" data-row="G" data-col="7"></td>
-        <td class="hidden" data-row="G" data-col="8"></td>
-    </tr>
-    <tr>
-        <th>H</th>
-        <td class="hidden" data-row="H" data-col="1"></td>
-        <td class="hidden" data-row="H" data-col="2"></td>
-        <td class="hidden" data-row="H" data-col="3"></td>
-        <td class="hidden" data-row="H" data-col="4"></td>
-        <td class="hidden" data-row="H" data-col="5"></td>
-        <td class="hidden" data-row="H" data-col="6"></td>
-        <td class="hidden" data-row="H" data-col="7"></td>
-        <td class="hidden" data-row="H" data-col="8"></td>
+        <td class="hidden"></td>
+        <td class="hidden"></td>
+        <td class="hidden"></td>
+        <td class="hidden"></td>
+        <td class="hidden"></td>
     </tr>
 </table>
 
