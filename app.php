6a25a6baf5439c66c010408d63c129b1bf05cb57
ZalohaD <dinis7794@gmail.com>
2024-06-29 21:25:49 +0300

    Fix css / create block for games

--- app.php
+++ app.php
@@ -12,8 +12,9 @@
         <div class="slot-wrapper">
             <h1>⚣ 300Bucks Casino Menu ⚣</h1>
 
-            <a href="slots.php">Slots</a>
-            <a href="mines.php">Mines</a>
+            <a class="game" href="slots.php">Slots</a>
+            <a class="game" href="mines.php">Mines</a>
+
 
 
         </div>
