e5b01e7715665b25064a53dc88d5c54b83c0c284
frstshmn <korenha.v@gmail.com>
2024-06-29 14:24:14 +0300

    Minor Update

--- app.php
+++ app.php
@@ -19,6 +19,5 @@
         </div>
         <script src="https://telegram.org/js/telegram-web-app.js"></script>
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-        <script src="assets/app.js"></script>
     </body>
 </html>
