2c2f4461f432c6375cdbf7cb6135f58f32477b89
frstshmn <korenha.v@gmail.com>
2024-06-29 22:54:13 +0300

    Minor Update

--- assets/slots.js
+++ assets/slots.js
@@ -19,7 +19,6 @@ $(document).ready(function() {
         } else {
             status = 1;
             $('#spin').prop('disabled', true);
-            $('#stop').prop('disabled', false);
         }
 
         if (balance < bet) {
@@ -64,48 +63,51 @@ $(document).ready(function() {
         }, 1000); // Start spinning after 1000 milliseconds
     });
 
-    $('#stop').click(function() {
-        setTimeout(function() {
-            $('#reel-1').removeClass('reel-spinning');
-            if (intervalId1) {
-                clearInterval(intervalId1);
-            }
-        }, 0); // Stop spinning immediately
-
-        setTimeout(function() {
-            $('#reel-2').removeClass('reel-spinning');
-            if (intervalId2) {
-                clearInterval(intervalId2);
-            }
-        }, 500); // Stop spinning after 500 milliseconds
-
-        setTimeout(function() {
-            $('#reel-3').removeClass('reel-spinning');
-            if (intervalId3) {
-                clearInterval(intervalId3);
-            }
-
-            setTimeout(function() {
-                let reel1 = $('#reel-1').text();
-                let reel2 = $('#reel-2').text();
-                let reel3 = $('#reel-3').text();
-
-                if (reel1 === reel2 && reel2 === reel3) {
-                    balance += bet * 10;
-                } else if (reel1 === reel2 || reel2 === reel3 || reel1 === reel3) {
-                    balance += bet * 1.5;
-                } else if (emojis.slice(0, 4).includes(reel1) && emojis.slice(0, 4).includes(reel2) && emojis.slice(0, 4).includes(reel3)) {
-                    balance += bet * 5;
-                }
-
-                $('#balance').text(balance);
-                $('input[name="bet"]').prop('disabled', false);
-
-                status = 0;
-                $('#spin').prop('disabled', false);
-                $('#stop').prop('disabled', true);
-
-            }, 500); // Stop spinning after 1000 milliseconds
-        }, 1000); // Stop spinning after 1000 milliseconds
-    });
+    setTimeout(
+        function () {
+            stopSpinning();
+            status = 0;
+        }, 3000);
 });
+
+function stopSpinning() {
+    setTimeout(function() {
+        $('#reel-1').removeClass('reel-spinning');
+        if (intervalId1) {
+            clearInterval(intervalId1);
+        }
+    }, 0); // Stop spinning immediately
+
+    setTimeout(function() {
+        $('#reel-2').removeClass('reel-spinning');
+        if (intervalId2) {
+            clearInterval(intervalId2);
+        }
+    }, 500); // Stop spinning after 500 milliseconds
+
+    setTimeout(function() {
+        $('#reel-3').removeClass('reel-spinning');
+        if (intervalId3) {
+            clearInterval(intervalId3);
+        }
+
+        setTimeout(function() {
+            let reel1 = $('#reel-1').text();
+            let reel2 = $('#reel-2').text();
+            let reel3 = $('#reel-3').text();
+
+            if (reel1 === reel2 && reel2 === reel3) {
+                balance += bet * 10;
+            } else if (reel1 === reel2 || reel2 === reel3 || reel1 === reel3) {
+                balance += bet * 1.5;
+            } else if (emojis.slice(0, 4).includes(reel1) && emojis.slice(0, 4).includes(reel2) && emojis.slice(0, 4).includes(reel3)) {
+                balance += bet * 5;
+            }
+
+            $('#balance').text(balance);
+            $('input[name="bet"]').prop('disabled', false);
+
+            $('#spin').prop('disabled', false);
+        }, 500); // Stop spinning after 1000 milliseconds
+    }, 1000); // Stop spinning after 1000 milliseconds
+}
--- slots.php
+++ slots.php
@@ -30,7 +30,6 @@
 
     <div class="controls">
         <button id="spin">Spin ⬇️</button>
-        <button id="stop" disabled>Stop ⏹️</button>
     </div>
 
     <div class="balance">
@@ -74,6 +73,6 @@
 </div>
 <script src="https://telegram.org/js/telegram-web-app.js"></script>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-<script src="assets/app.js"></script>
+<script src="assets/slots.js"></script>
 </body>
 </html>
