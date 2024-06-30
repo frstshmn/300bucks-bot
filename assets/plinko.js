e8e59edb05b6313a8867c5f0d6e7c5e1428f35b2
ZalohaD <dinis7794@gmail.com>
2024-06-30 14:05:53 +0300

    Minor fix

--- assets/plinko.js
+++ assets/plinko.js
@@ -1,47 +1,77 @@
 Telegram.WebApp.ready();
 
-    // Matter.js module aliases
-    var Engine = Matter.Engine,
+// Matter.js module aliases
+var Engine = Matter.Engine,
     Render = Matter.Render,
     World = Matter.World,
     Bodies = Matter.Bodies,
     Events = Matter.Events;
 
-    // Create an engine
-    var engine = Engine.create();
+// Create an engine
+var engine = Engine.create();
 
-    // Create a renderer
-    var render = Render.create({
+// Create a renderer
+var render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-    width: 800,
-    height: 600,
-    wireframes: false
-}
+        width: 800,
+        height: 600,
+        wireframes: false
+    }
 });
 
-    // Create bodies
-    var ground = Bodies.rectangle(400, 600, 800, 50, { isStatic: true });
+// Create bodies
+var ground = Bodies.rectangle(400, 600, 800, 50, { isStatic: true });
 
-    var pegs = [];
-    for (var i = 50; i <= 750; i += 50) {
+var pegs = [];
+for (var i = 50; i <= 750; i += 50) {
     for (var j = 75; j <= 350; j += 100) {
-    var peg = Bodies.circle(i + (j % 100 ? 25 : 0), j, 10, { isStatic: true });
-    pegs.push(peg);
-}
+        var peg = Bodies.circle(i + (j % 100 ? 25 : 0), j, 10, { isStatic: true });
+        pegs.push(peg);
+    }
 }
 
-    var boundaries = [
+var boundaries = [
     Bodies.rectangle(0, 300, 20, 600, { isStatic: true }),
     Bodies.rectangle(800, 300, 20, 600, { isStatic: true })
-    ];
+];
 
-    // Add all of the bodies to the world
-    World.add(engine.world, [ground, ...pegs, ...boundaries]);
+// Add all of the bodies to the world
+World.add(engine.world, [ground, ...pegs, ...boundaries]);
 
-    // Run the engine
-    Engine.run(engine);
+// Balance and multiplier variables
+var balance = 100;
+var multiplier = 1;
 
-    // Run the renderer
-    Render.run(render);
+// Function to throw the ball
+function throwBall() {
+    if (balance > 0) {
+        balance--;
+        updateBalanceDisplay();
+        var ball = Bodies.circle(400, 0, 10, {
+            restitution: 1,
+            friction: 0,
+            frictionAir: 0.01
+        });
+        World.add(engine.world, [ball]);
+    } else {
+        alert("Out of balance! Reset to continue playing.");
+    }
+}
+
+// Function to update balance display
+function updateBalanceDisplay() {
+    document.getElementById('balance').value = balance;
+}
+
+// Event handling for multiplier input
+document.getElementById('multiplier').addEventListener('change', function() {
+    multiplier = parseInt(this.value) || 1;
+});
+
+// Run the engine
+Engine.run(engine);
+
+// Run the renderer
+Render.run(render);
