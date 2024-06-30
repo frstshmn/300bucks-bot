bb41a55322da68fa2b38f3e38a63a575695f2808
ZalohaD <dinis7794@gmail.com>
2024-06-30 18:36:36 +0300

    Minor fix

--- assets/plinko.js
+++ assets/plinko.js
@@ -145,13 +145,15 @@ Events.on(render, 'afterRender', function() {
     context.font = '24px Arial';
     context.fillStyle = 'black';
     context.textAlign = 'center';
+
     for (var i = 0; i < slots.length; i++) {
         var slot = slots[i];
         var text = slot.multiplier + 'x';
         var textWidth = context.measureText(text).width;
         var x = slot.position.x;
-        var y = slotY + slotHeight + 30; // Adjusted position to be below the slots
+        var y = slotY + slotHeight + 100; // Adjusted position to be below the slots
 
         context.fillText(text, x, y);
     }
 });
+
