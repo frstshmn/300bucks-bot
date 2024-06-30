c05eb94162ff3a56d2ce1c64f56ac4bc322f9f55
ZalohaD <dinis7794@gmail.com>
2024-06-30 12:28:11 +0300

    Minor fix

--- plinko.php
+++ plinko.php
@@ -31,6 +31,8 @@
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/matter-js/0.17.1/matter.min.js"></script>
 <script src="https://telegram.org/js/telegram-web-app.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
 
 <script src="assets/plinko.js"></script>
 </body>
