bc445ee2391a6d3fca1b7c0ec44c136d9a00facd
ZalohaD <dinis7794@gmail.com>
2024-06-30 19:52:14 +0300

    Minor fix

--- aviator.php
+++ aviator.php
@@ -9,11 +9,11 @@
 <body>
 <div id="game-container__aviator">
 
-        <div id="multiplier">Multiplier: <span id="multiplier-value">1.00x</span></div>
-        <div id="airplane-placeholder">✈️</div>
-        <button id="start-button">Start</button>
-        <button id="cashout-button" disabled>Cashout</button>
-        <div id="balance">Balance: $<span id="balance-value">300</span></div>
+    <div id="multiplier">Multiplier: <span id="multiplier-value">1.00x</span></div>
+    <div id="airplane-placeholder">✈️</div>
+    <button id="start-button">Start</button>
+    <button id="cashout-button" disabled>Cashout</button>
+    <div id="balance">Balance: $<span id="balance-value">300</span></div>
 
 </div>
 
