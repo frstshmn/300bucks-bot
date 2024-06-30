99fc9cf8070bb37e845471c9003efb2898e54d0f
ZalohaD <dinis7794@gmail.com>
2024-06-30 14:17:02 +0300

    Minor fix

--- assets/plinko.js
+++ assets/plinko.js
@@ -24,35 +24,52 @@ var render = Render.create({
 // Create bodies
 var ground = Bodies.rectangle(400, 600, 800, 50, { isStatic: true });
 
+// Create pegs
 var pegs = [];
 for (var i = 50; i <= 750; i += 50) {
-    for (var j = 75; j <= 350; j += 100) {
+    for (var j = 75; j <= 300; j += 50) {
         var peg = Bodies.circle(i + (j % 100 ? 25 : 0), j, 10, { isStatic: true });
         pegs.push(peg);
     }
 }
 
+// Create multiplier tiles with corresponding chances
 var multiplierTiles = [];
-var multiplierValues = [0, 0, 0, 0, 0, 0, 0, 1, 1, 2]; // Sample chances from Stake.com
-for (var k = 0; k < 10; k++) {
-    var multiplierTile = Bodies.rectangle(50 + k * 80, 450, 60, 40, {
+var multiplierChances = [0.125, 0.125, 0.25, 0.25, 0.125, 0.125];
+var multiplierValues = [0, 0, 0, 1, 1, 2];
+var xPos = 40;
+
+for (var i = 0; i < multiplierChances.length; i++) {
+    var tileHeight = 50 + 50 * i;
+    var multiplierTile = Bodies.rectangle(xPos, tileHeight, 60, 40, {
         isStatic: true,
-        label: "multiplier",
-        multiplier: multiplierValues[k]
+        label: 'multiplier',
+        multiplier: multiplierValues[i]
     });
     multiplierTiles.push(multiplierTile);
+    xPos += 80;
 }
 
+// Create obstacles (bumpers)
+var obstacles = [
+    Bodies.rectangle(200, 100, 180, 20, { isStatic: true, angle: Math.PI * 0.06 }),
+    Bodies.rectangle(600, 100, 180, 20, { isStatic: true, angle: -Math.PI * 0.06 }),
+    Bodies.rectangle(400, 300, 300, 20, { isStatic: true, angle: Math.PI * 0.1 }),
+    Bodies.rectangle(200, 500, 200, 20, { isStatic: true, angle: -Math.PI * 0.1 }),
+    Bodies.rectangle(600, 500, 200, 20, { isStatic: true, angle: Math.PI * 0.1 })
+];
+
+// Boundaries
 var boundaries = [
     Bodies.rectangle(0, 300, 20, 600, { isStatic: true }),
     Bodies.rectangle(800, 300, 20, 600, { isStatic: true })
 ];
 
-// Add all of the bodies to the world
-World.add(engine.world, [ground, ...pegs, ...multiplierTiles, ...boundaries]);
+// Add all bodies to the world
+World.add(engine.world, [ground, ...pegs, ...multiplierTiles, ...obstacles, ...boundaries]);
 
 // Balance and multiplier variables
-var balance = 300; // Default balance set to 300 dollars
+var balance = 300;
 var multiplier = 1;
 
 // Function to throw the ball
@@ -68,7 +85,7 @@ function throwBall() {
         });
         World.add(engine.world, [ball]);
 
-        // Detect when the ball hits a multiplier tile
+        // Event handling for ball hitting multiplier tiles
         Events.on(engine, 'collisionStart', function(event) {
             var pairs = event.pairs;
             for (var i = 0; i < pairs.length; i++) {
