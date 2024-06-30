a8653191ef53e0b3f69713e1fd3915086baabd9a
ZalohaD <dinis7794@gmail.com>
2024-06-30 19:20:06 +0300

    Minor fix

--- plinko.php
+++ plinko.php
@@ -11,14 +11,14 @@
 
 <canvas id="plinkoCanvas"></canvas>
 
+<label for="balance">Balance:</label>
+<span id="balanceDisplay">300</span>
+
 <div class="navbar">
     <div class="amount">
         <label for="bet">Bet Amount: </label>
         <input type="number" id="bet" min="1" value="1">
     </div>
-
-    <label for="balance">Balance:</label>
-    <span id="balanceDisplay">300</span>
 
     <button onclick="throwBall()">Throw Ball</button>
 
