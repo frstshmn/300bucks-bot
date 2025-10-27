71e32baca104df99930a3267db5bed2118b87b26
ZalohaD <dinis7794@gmail.com>
2025-10-27 22:10:12 +0200

    Fix auth

--- app.php
+++ app.php
@@ -282,11 +282,11 @@ if (isset($_SESSION['telegram_id'])) {
     </div>
     <div style="margin-top:12px; text-align:center;">
         <?php if (!$user): ?>
-            <script async src="https://telegram.org/js/telegram-widget.js?22"
-                    data-telegram-login="threehunderedbucks_bot"
-                    data-size="large"
-                    data-auth-url="auth.php"
-                    data-request-access="write">
+            <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="threehunderedbucks_bot" data-size="medium" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
+            <script type="text/javascript">
+                function onTelegramAuth(user) {
+                    alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+                }
             </script>
         <?php else: ?>
             <div style="display:flex; justify-content:center; align-items:center; gap:12px;">
